62725553f3a9535acc95a1a21bed37f1f234c434
Haodong Li <haodongli@users.noreply.huggingface.co>
2023-11-27 09:41:43 +0000

    Update gradio_demo.py

--- gradio_demo.py
+++ gradio_demo.py
@@ -6,13 +6,15 @@ try:
     from train import *
     print('==> simple-knn & diff-gaussian-rasterization already installed!')
 except:
+    print('==> simple-knn & diff-gaussian-rasterization are NOT installed!')
     # https://github.com/pytorch/extension-cpp/issues/71
     os.environ["TORCH_CUDA_ARCH_LIST"] = "3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
     print('==> TORCH_CUDA_ARCH_LIST =', os.environ.get('TORCH_CUDA_ARCH_LIST'))
     os.system("python -m pip install git+https://github.com/YixunLiang/simple-knn.git")
     print('==> simple-knn installed!')
     os.system("python -m pip install git+https://github.com/YixunLiang/diff-gaussian-rasterization.git")
-    print('==> diff-gaussian-rasterization installed!')    
+    print('==> diff-gaussian-rasterization installed!')
+    from train import *
 
 example_inputs = [[
         "A DSLR photo of a Rugged, vintage-inspired hiking boots with a weathered leather finish, best quality, 4K, HD.",
